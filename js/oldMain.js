db1538695f7a7ef3dd56d33fa65a5f53a6094606
Lucas Real <96266377+Lucas-VReal@users.noreply.github.com>
2022-10-20 16:03:43 -0300

    meu primeiro commit no projeto robotron

---
+++ js/oldMain.js
@@ -0,0 +1,44 @@
+// const robotron = document.querySelector("#robotron");
+
+// robotron.addEventListener("click", () => {
+//     console.log("Cliquei no robô");
+// });
+
+// robotron.addEventListener("click", (evento) => {
+//     console.log(evento);
+// });
+
+// function dizOi (nome){
+//     console.log("Oi, " + nome);
+//     console.log("Bem vindo ao Robotron 2000")
+// };
+
+// dizOi("Pedro");
+
+// somar.addEventListener("click", () =>{
+//     // braco.value = parseInt(braco.value) + 1;
+//     braco.value++;
+// })
+
+// subtrair.addEventListener("click", () =>{
+//     // braco.value = parseInt(braco.value) - 1;
+//     braco.value--;
+// })
+
+// function aumentar (element, equipament){
+//     element.addEventListener("click", () =>{
+//         equipament.value++;
+//     })
+// }
+
+// function diminuir (element, equipament){
+//     element.addEventListener("click", () =>{
+//         if (equipament.value != 0){
+//             equipament.value--;
+//         }
+//     })
+// }
+
+// aumentar(somar, braco);
+
+// diminuir(subtrair, braco);
